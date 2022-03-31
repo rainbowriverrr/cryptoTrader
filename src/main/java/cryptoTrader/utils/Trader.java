9a08a9cafff87a6869febc5e77e255898aac714c
rainbowriverrr <93089849+rainbowriverrr@users.noreply.github.com>
2022-03-30 23:21:52 -0400

    Trader stub

--- src/main/java/cryptoTrader/utils/Trader.java
+++ src/main/java/cryptoTrader/utils/Trader.java
@@ -1,4 +1,25 @@
 package cryptoTrader.utils;
 
+import java.util.ArrayList;
+
 public class Trader {
+
+    private static AvailableCryptoList coinsAvailable;
+
+    public static void performTrades(ArrayList<TradingClient> clients){
+
+        coinsAvailable = AvailableCryptoList.getInstance();
+        coinsAvailable.call();
+
+        String[] availableCoinsList = coinsAvailable.getAvailableCryptos();
+
+        ArrayList<LogItem> logs;
+
+        for(TradingClient client : clients) {
+            String[] coins = client.getCryptoCoins();
+            for(String coin : coins){
+
+            }
+        }
+    }
 }
